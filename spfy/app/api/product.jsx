8695eb81faf0f591fe209948f87b052fcfde26af
Min <min@magestore.com>
2023-08-04 08:10:33 +0700

    update

--- spfy/app/api/product.jsx
+++ spfy/app/api/product.jsx
@@ -1,48 +1,22 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// const ShopifyAPIExample = () => {
-//   const [data, setData] = useState(null);
-//   useEffect(() => {
-//     // Replace 'YOUR_SHOPIFY_STORE_URL' with your actual Shopify store URL
-//     const shopifyStoreUrl = "YOUR_SHOPIFY_STORE_URL";
-//     const accessToken = "YOUR_SHOPIFY_ACCESS_TOKEN";
-//     const graphqlQuery = `
-//       {
-//         shop {
-//           name
-//           description
-//         }
-//       }
-//     `;
-//     axios
-//       .post(
-//         `https://${shopifyStoreUrl}/admin/api/2021-07/graphql.json`,
-//         { query: graphqlQuery },
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             "X-Shopify-Access-Token": accessToken,
-//           },
-//         }
-//       )
-//       .then((response) => {
-//         setData(response.data.data.shop);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//       });
-//   }, []);
-//   return (
-//     <div>
-//       {data ? (
-//         <>
-//           <h2>Shop Name: {data.name}</h2>
-//           <p>Shop Description: {data.description}</p>
-//         </>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-// };
-// export default ShopifyAPIExample;
+import axios from "axios";
+
+export async function callShopifyGraphQL(query) {
+  const endpoint = "https://spfy8.myshopify.com/admin/api/2023-07/graphql.json";
+  const apiKey = "21e70e1c0db438b03bf85dafbc7b9e85";
+  const accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczpcL1wvc3BmeTgubXlzaG9waWZ5LmNvbVwvYWRtaW4iLCJkZXN0IjoiaHR0cHM6XC9cL3NwZnk4Lm15c2hvcGlmeS5jb20iLCJhdWQiOiIyMWU3MGUxYzBkYjQzOGIwM2JmODVkYWZiYzdiOWU4NSIsInN1YiI6IjEwMTg3MTEyNDc3OCIsImV4cCI6MTY5MTA3NzUyMiwibmJmIjoxNjkxMDc3NDYyLCJpYXQiOjE2OTEwNzc0NjIsImp0aSI6IjEzZmYxOGQ2LWQ0NGMtNGFmZi04ZWZkLTYwMzhhZDBmYzliYiIsInNpZCI6IjUwZWM2YTc2MjQ3ZTA4ZjA2YTdmMzlhODc5ZDExZTY4YTczZWY2YjgyMDE1NjY4N2Q3MzFjYWIzZjRlOTY4YmMifQ.eTeq1dr-GTkYOGL2QLy4y3g3aqm2vveVMDOwaRfdZ18";
+  const headers = {
+    "X-Shopify-Access-Token": accessToken,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const response = await axios.post(endpoint, { query }, { headers });
+    console.log("RESTPONSE: ", response);
+
+    return response.data.data; // Trả về dữ liệu từ phản hồi GraphQL
+  } catch (error) {
+    console.error("Error calling Shopify GraphQL API:", error);
+    throw error;
+  }
+}
